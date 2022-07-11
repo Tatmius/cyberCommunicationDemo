fec188a011e1110651eaf02764ed01bbe2418b8f
tatmius <kddiw.comusodayo@gmail.com>
2022-07-12 06:41:52 +0900

    insert twitch

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ function startLoad() {
   startButton.remove();
   let youtubeFrameDiv = document.getElementById('youtubeFrame');
   let nicoVideoFrameDiv = document.getElementById('nicoVideoFrame');
-  //let twitchFrameDiv = document.getElementById('twitchFrame');
+  let twitchFrameDiv = document.getElementById('twitchFrame');
   let rumbleFrameDiv = document.getElementById('rumbleFrame');
   let localVideoDiv = document.getElementById('localVideo');
 
   const youtubeFrame = '<iframe width="350" onload="iframeLoaded()" src="https://www.youtube.com/embed/gEYNgdvcdLs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
   const nicoVideoFrame = '<iframe width="350" onload="iframeLoaded()" src="https://embed.nicovideo.jp/watch/sm40683783"></iframe>';
-  //const twitchFrame = '<iframe src="https://clips.twitch.tv/embed?clip=TenaciousFunnyWaspKeepo-wYjv4YsAOY0_AThZ&parent=219.121.57.250" frameborder="0" allowfullscreen="true" scrolling="no" width="350"></iframe>'
+  const twitchFrame = '<iframe src="https://clips.twitch.tv/embed?clip=TenaciousFunnyWaspKeepo-wYjv4YsAOY0_AThZ&parent=cybercomdemo.tatmius.tk" frameborder="0" allowfullscreen="true" scrolling="no" width="350"></iframe>'
   const rumbleFrame = '<iframe class="rumble" onload="iframeLoaded()" width="350" src="https://rumble.com/embed/v18p5pn/?pub=4" frameborder="0" allowfullscreen></iframe>';
   const localVideo = '<video controls width="350"><source src="localVideo.mp4" type="video/mp4" playsinline></video>'
   youtubeFrameDiv.insertAdjacentHTML('afterbegin', youtubeFrame);
   nicoVideoFrameDiv.insertAdjacentHTML('afterbegin', nicoVideoFrame);
-  //twitchFrameDiv.insertAdjacentHTML('afterbegin', twitchFrame);
+  twitchFrameDiv.insertAdjacentHTML('afterbegin', twitchFrame);
   rumbleFrameDiv.insertAdjacentHTML('afterbegin', rumbleFrame);
   localVideoDiv.insertAdjacentHTML('afterbegin',localVideo )
 
