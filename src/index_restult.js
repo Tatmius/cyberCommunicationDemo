00c8fac55b1fc84f6907979107cca2b5bdd2a5ad
root <root@cyberCom.vmm.proxmox>
2022-07-12 19:06:45 +0900

    change color

--- src/index_restult.js
+++ src/index_restult.js
@@ -1,8 +1,31 @@
+const typeArr = ["youtube","niconico","twitch","rumble","local"];
+
 function getResult(){
   fetch("https://cybercomdemo.tatmius.tk/duration/ave", {
     method: "GET",
+    headers: {
+      Accept: 'application/json',
+    },
   }).then(response => {
     console.log(response.status); // 多分200
     console.log(response.headers.get('Content-Encoding')); 
+    response.json().then(json => {
+      insertResult(json);
+    });
   })
 }
+
+function insertResult(result){
+  const data = JSON.parse(result);
+  const resultDiv = document.getElementById('Result');
+  for(const element of typeArr){
+    result = data[element];
+    resultDiv.insertAdjacentHTML('beforeend',formatData(result));
+  }
+}
+
+function formatData(data){
+  return '<p class="text-2xl text-white">'+String(data)+'[ms]</p>'
+}
+
+getResult();
